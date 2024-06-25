448dcc3d0ea468b44de32b5916802876649e674a
Litto <118559955+kaittozu@users.noreply.github.com>
2024-06-25 08:28:01 +0700

    Add files via upload

---
+++ Stopwatch Project/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace asdas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBTN = new System.Windows.Forms.Button();
+            this.pauseBTN = new System.Windows.Forms.Button();
+            this.resetBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gotham Thin", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(74, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(668, 101);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startBTN
+            // 
+            this.startBTN.BackColor = System.Drawing.Color.MediumBlue;
+            this.startBTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBTN.ForeColor = System.Drawing.Color.White;
+            this.startBTN.Location = new System.Drawing.Point(71, 171);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(160, 60);
+            this.startBTN.TabIndex = 1;
+            this.startBTN.Text = "Start";
+            this.startBTN.UseVisualStyleBackColor = false;
+            this.startBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pauseBTN
+            // 
+            this.pauseBTN.BackColor = System.Drawing.Color.MediumPurple;
+            this.pauseBTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.pauseBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseBTN.ForeColor = System.Drawing.Color.White;
+            this.pauseBTN.Location = new System.Drawing.Point(319, 171);
+            this.pauseBTN.Name = "pauseBTN";
+            this.pauseBTN.Size = new System.Drawing.Size(160, 60);
+            this.pauseBTN.TabIndex = 2;
+            this.pauseBTN.Text = "Pause";
+            this.pauseBTN.UseVisualStyleBackColor = false;
+            this.pauseBTN.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resetBTN
+            // 
+            this.resetBTN.BackColor = System.Drawing.Color.Red;
+            this.resetBTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.resetBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBTN.ForeColor = System.Drawing.Color.White;
+            this.resetBTN.Location = new System.Drawing.Point(562, 171);
+            this.resetBTN.Name = "resetBTN";
+            this.resetBTN.Size = new System.Drawing.Size(160, 60);
+            this.resetBTN.TabIndex = 3;
+            this.resetBTN.Text = "Reset";
+            this.resetBTN.UseVisualStyleBackColor = false;
+            this.resetBTN.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(38)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(786, 300);
+            this.Controls.Add(this.resetBTN);
+            this.Controls.Add(this.pauseBTN);
+            this.Controls.Add(this.startBTN);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Button pauseBTN;
+        private System.Windows.Forms.Button resetBTN;
+    }
+}
+
